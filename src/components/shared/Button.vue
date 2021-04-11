40af4b000aef2240f0ee5d7d734f41b87b214051
Yura Korol <yrakorol200012@gmail.com>
2021-04-11 17:15:23 +0300

    add button

---
+++ src/components/shared/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <button :style="{
+    backgroundColor: bgColor,
+    width: `${width}%`,
+    height: `${height}px`,
+    color: textColor,
+    borderRadius: `${borderRadius}px`
+  }">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 100
+    },
+    height: {
+      type: Number,
+      default: 25
+    },
+    bgColor: {
+      type: String,
+      default: 'transparent'
+    },
+    textColor: {
+      type: String,
+      default: 'black'
+    },
+    borderRadius: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    border: none;
+    font-size: 15px;
+    outline: none;
+    cursor: pointer;
+  }
+</style>
