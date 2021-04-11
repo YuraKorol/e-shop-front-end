32f766747852103ea26f0daa873ceb15f2af0f1f
Yura Korol <yrakorol200012@gmail.com>
2021-04-11 16:30:57 +0300

    add product-services

---
+++ src/services/products.service.js
@@ -0,0 +1,16 @@
+import { request } from './generic.sevice'
+
+const getAllProducts = () => request({ url: 'products/', method: 'get' })
+
+const getOneProduct = id => request({ url: `products/${id}`, method: 'get' })
+
+const updateProduct = id => request({ url: `products/${id}`, method: 'post' })
+
+const deleteProduct = id => request({ url: `products/${id}`, method: 'delete' })
+
+export {
+  getAllProducts,
+  getOneProduct,
+  updateProduct,
+  deleteProduct
+}
