58d1c2255fbfce77ff2b3bbde2d8304c2d166c81
Yura Korol <yrakorol200012@gmail.com>
2021-04-11 16:34:43 +0300

    add header

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header_container">
+    <div>
+      Logo
+    </div>
+    <Navigation :categories='categories' />
+  </header>
+</template>
+
+<script>
+import { reactive } from 'vue'
+import Navigation from './Navigation.vue'
+
+export default {
+  setup() {
+    const categories = reactive([
+      { title: 'PC', id: 1 },
+      { title: 'PS4', id: 2 },
+      { title: 'XBox', id: 3 }
+    ])
+    return {
+      categories
+    }
+  },
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style scoped>
+  .header_container {
+    display: flex;
+    align-items: center;
+    padding: 15px;
+    box-shadow: 0 6px 4px -3px rgba(0, 0, 0, .4);
+  }
+</style>
---
+++ src/components/layouts/Navigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="nav_list_container">
+    <router-link
+      :to="`/category/${category.id}`"
+      tag="li"
+      class="nav_link"
+      v-for="category in categories"
+      :key="category.id"
+    > {{ category.title }} </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .nav_list_container {
+    margin: 0 auto;
+    list-style: none;
+  }
+  .nav_link {
+    margin: 0px 10px;
+    color: black;
+    text-decoration: none;
+  }
+</style>
