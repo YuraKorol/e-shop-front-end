da27c4f097946f50707f2f1e0f4f4f8e5a8ae03e
Yura Korol <yrakorol200012@gmail.com>
2021-04-28 09:06:28 +0300

    add input

---
+++ src/components/shared/Input.vue
@@ -0,0 +1,48 @@
+<template>
+  <input
+    :type="type"
+    :style="{
+      width: `${width}%`,
+      height: `${height}px`
+    }"
+    :placeholder="placeholder"
+    :value="value"
+    @input="$emit('update:value', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 100
+    },
+    height: {
+      type: Number,
+      default: 15
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input {
+    border: 1px solid #dcdfe6;
+    border-radius: 6px;
+    padding: 5px;
+    outline: none;
+  }
+</style>
